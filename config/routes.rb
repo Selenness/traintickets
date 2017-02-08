2599217930ce4a4dff259cdaeb972885dc455c9a
selenness <833117@mail.ru>
2017-02-08 14:13:26 +0500

    задание 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :trains
   resources :railway_stations
+  root 'trains#index'
+  resources :routes
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
