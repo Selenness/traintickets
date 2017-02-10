449d80165a5a1abb86991063a0ec5df1922f9bdc
selenness <833117@mail.ru>
2017-02-10 08:20:48 +0500

    задание 12 исправления

--- db/migrate/20170209091940_create_tickets.rb
+++ db/migrate/20170209091940_create_tickets.rb
@@ -1,7 +1,7 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
-      t.belongs_to :tickets, :start_station, index: true
+      t.belongs_to :tickets, :first_station, index: true
       t.belongs_to :tickets, :end_station, index: true
 
       t.timestamps
