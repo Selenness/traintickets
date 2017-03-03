0f96d84fb4fe5a249d69335cd18bab5aced78a9f
selenness <833117@mail.ru>
2017-03-03 19:16:29 +0500

    задание 19 убран лишний индекс из миграций

--- db/migrate/20170220045026_add_index_to_railway_station_id_in_train.rb
+++ db/migrate/20170220045026_add_index_to_railway_station_id_in_train.rb
@@ -1,5 +1,5 @@
 class AddIndexToRailwayStationIdInTrain < ActiveRecord::Migration[5.0]
   def change
-    add_index :trains, :current_station_id
+    # add_index :trains, :current_station_id
   end
 end
