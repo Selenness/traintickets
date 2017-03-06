9e1ea664dfbb280f702ac5cdc4a3cdc6e1756796
selenness <833117@mail.ru>
2017-03-06 17:17:48 +0500

    задание 19 удален апликейшн.вмл

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'devise'
 gem 'therubyracer'
 gem 'twitter-bootstrap-rails'
 gem 'rails_12factor'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,15 +57,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "traintickets_#{Rails.env}"
   config.action_mailer.default_url_options = { host: 'afternoon-fjord-90910.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "myapp.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+  }
   # SMTP settings for mailgun
-  # ActionMailer::Base.smtp_settings = {
-  #     :port           => 587,
-  #     :address        => "smtp.mailgun.org",
-  #     :domain         => ENV['domain'],
-  #     :user_name      => ENV['username'],
-  #     :password       => ENV['password'],
-  #     :authentication => :plain,
-  # }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
